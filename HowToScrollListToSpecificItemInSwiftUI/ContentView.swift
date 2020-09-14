8fb7a571e22617267a486ef57a635d33ee76ab0c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-14 20:02:50 +0300

    Initial Commit

---
+++ HowToScrollListToSpecificItemInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowToScrollListToSpecificItemInSwiftUI
+//
+//  Created by Ramill Ibragimov on 14.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowToScrollListToSpecificItemInSwiftUI/HowToScrollListToSpecificItemInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToScrollListToSpecificItemInSwiftUIApp.swift
+//  HowToScrollListToSpecificItemInSwiftUI
+//
+//  Created by Ramill Ibragimov on 14.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToScrollListToSpecificItemInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
