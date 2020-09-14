818f7eea8d382eb528e4a5e79635465367eed796
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-14 20:14:14 +0300

    current position with timer publisher

--- HowToScrollListToSpecificItemInSwiftUI/ContentView.swift
+++ HowToScrollListToSpecificItemInSwiftUI/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let publisher = Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect()
+    
+    @State private var currentPosition = 20
+    
     var body: some View {
         ScrollViewReader { scrollViewReader in
             List(1...100, id: \.self) { item in
                 Text("Item \(item)")
                     .onTapGesture(count: 1, perform: {
                         withAnimation {
-                            scrollViewReader.scrollTo(item)
+                            scrollViewReader.scrollTo(item, anchor: .top)
                         }
                     })
             }
+            .onReceive(publisher, perform: { _ in
+                currentPosition += 1
+                
+                withAnimation {
+                    scrollViewReader.scrollTo(currentPosition, anchor: .top)
+                }
+            })
         }
     }
 }
